515a5b9033c9c640944a5cbcd9a969d7362d4ff7
SashaTalk <76222964+SashaTalk@users.noreply.github.com>
2023-05-04 21:52:58 +0300

    fix: cat.lua

--- cat.lua
+++ cat.lua
@@ -3,9 +3,6 @@ file = fs.read(thread, fs.size("cat.lua"))
 fs.close(thread)
 lines = {}
 for s in string.gmatch(file, "[^\n]+") do
-    table.insert(lines, s)
-end
-for i in pairs(lines) do
-    gpu.set(1, cursor.y, tostring(i))
+    gpu.set(1, cursor.y, s)
     cursor.y = cursor.y + 1
 end
